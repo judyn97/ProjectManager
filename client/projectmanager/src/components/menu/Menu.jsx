dc13a75484f74c288a7c1674758f25040409b8c4
jalaludinzakaria <jalaludin.zakaria@music.yamaha.com>
2024-11-13 18:47:04 +0800

    Revise minor functions
    - Implement active state for menu buttons
    - Revised 'Add Column' button size

--- client/projectmanager/src/components/menu/Menu.jsx
+++ client/projectmanager/src/components/menu/Menu.jsx
@@ -1,15 +1,19 @@
 import "./Menu.css";
+import { useState } from "react";
 import {Link} from "react-router-dom";
 import {menu} from "../../menudata"
 
 const Menu = () => {
+  const [selectedItem, setSelectedItem] = useState();
+  console.log("Select", selectedItem)
+
   return (
     <div className="menu">
       {menu.map((item)=>(
         <div className="item" key={item.id}>
           <span className="title">{item.title}</span>
           {item.listItems.map((listItem)=>(
-            <Link className="listItem" to={listItem.url} key={listItem.id}>
+            <Link className={(selectedItem === listItem.title) ? 'active listItem' : 'listItem'} to={listItem.url} key={listItem.id} onClick={() => setSelectedItem(listItem.title)}>
             <img src={listItem.icon} alt="" />
             <span className="listItemTitle">{listItem.title}</span>
             </Link>
