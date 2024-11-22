18830ac3a5a35f7456c6c2cdf70398320fe7370e
jalaludinzakaria <jalaludin.zakaria@music.yamaha.com>
2024-11-22 17:03:55 +0800

    Revise import mistake

--- client/projectmanager/src/pages/home/Home.jsx
+++ client/projectmanager/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './home.css';
+import './Home.css';
 import ProgressCard from './ProgressCard';
 import EventsTable from './Events.jsx';
 import UpcomingTask from './UpcomingTask.jsx';
